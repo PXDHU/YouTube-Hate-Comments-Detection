f67792640c349b7a5eb5a918160d571bbaed1fa2
PXDHU <padmavasan.contact@gmail.com>
2025-11-18 13:40:45 +0530

    data ingestion and preprocess

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Youtube Hate Comments Detection",
+    version="0.0.0",
+    author="Padmavasan Balakrishnan",
+    author_email="padmavasan.contact@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
